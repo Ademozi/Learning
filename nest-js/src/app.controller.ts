17a458a3abddb188895ecc18b3fa1667c171b4f3
Ademozi <ademoviadem@gmail.com>
2026-09-10 11:40:44 +0100

    post with a query parameters

--- nest-js/src/app.controller.ts
+++ nest-js/src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Query } from '@nestjs/common';
 import { AppService } from './app.service.js';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/hello')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // localhost:3000/welcome/?name=Adem
+  // It return an object { "name": "Adem" }
+  @Post('/welcome')
+  postWelcome(@Query() query: any) {
+    return query;
+    return this.appService.sayWelcomeToTheUser();
   }
 }
--- nest-js/src/app.service.ts
+++ nest-js/src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  sayWelcomeToTheUser(): string {
+    return 'Welcome to';
+  }
 }
